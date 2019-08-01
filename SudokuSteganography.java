a56bc88fb06d57f3b11ee1bea4f23f58a2f50bd4
Ash <a.patra@warwick.ac.uk>
2019-08-01 20:03:18 +0100

    Added file for framework

---
+++ SudokuSteganography.java
@@ -0,0 +1,20 @@
+
+import java.io.File;
+
+import java.awt.Point;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
+import java.awt.image.DataBufferByte;
+
+import javax.imageio.ImageIO;
+import javax.swing.JOptionPane;
+
+
+public class SudokuSteganography{
+
+  public boolean encode(String filePath, String coverImage, String extension, String output, String secret){
+    
+  }
+
+}
